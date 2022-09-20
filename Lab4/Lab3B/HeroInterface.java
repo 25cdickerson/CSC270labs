3990b03c20b364be1468c27c8d5d4e8ea6af6e44
Preston Dickerson <preston.dickerson@linuxlab.centre.edu>
2022-09-20 11:26:02 -0400

    Lab 4 Completed

--- Lab4/Lab3B/HeroInterface.java
+++
@@ -1,9 +0,0 @@
-public interface HeroInterface
-{
-   public int attack(int val);
-   public int getSTR();
-   public int getDEX();
-   public int getINT();
-   public String getRaceName();
-   public String getJobName();
-}
