847c611018a65e09b166b4497305ace7b8718a2e
Preston Dickerson <preston.dickerson@linuxlab.centre.edu>
2022-10-11 10:39:12 -0400

    Checkpoint Lab 6

--- Lab6/Queue.java
+++ Lab6/Queue.java
@@ -2,18 +2,24 @@ public class Queue<T>
 {
     //be sure that your attributes are protected, so subclasses can use them
 
-
-   public Queue()
-   {
+   private Node<T> head = null;
+   private Node<T> tail = null;
    
-   }
+   public Queue(){}
 
 
     /**Adds val to the end of the queue
      */
    public void push(T val)
    {
-   
+      if(tail == null){
+         head = new Node<T>(val, head);
+         tail = head;
+         return;
+      }
+      
+      tail.next = new Node<T>(val, null);
+      tail = tail.next;
    }
 
 
@@ -23,7 +29,18 @@ public class Queue<T>
      */
    public T pop()
    {
-       return null;
+       if(head == null){
+         throw new QueueUnderFlowException("Cannot dequeue empty queue");
+       }
+       
+       T temp = head.val;
+       head = head.next;
+       
+       if(head == null){
+         tail = null;
+       }
+       
+       return temp;
    }    
 
 
@@ -33,9 +50,19 @@ public class Queue<T>
 
    public boolean isEmpty()
    {
-       return false;
+       return head == null;
    }
 
+   
+   private class Node<T>{
+      private Node next;
+      private T val;
+      
+      public Node(T val, Node next){
+         this.next = next;
+         this.val = val;
+      }
+   }
 
 
 
